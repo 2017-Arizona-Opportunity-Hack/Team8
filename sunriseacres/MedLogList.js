c6d40fd9fd8ebdc1378ca8355798e24b4b210b84
Shu Sia Lukito <lukitos@yahoo.com>
2017-10-21 16:11:13 -0700

    Updated Medication Log

--- sunriseacres/MedLogList.js
+++ sunriseacres/MedLogList.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
 
 import MedLog from './MedLog';
 
 class MedLogList extends Component {
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
   render () {
     return (
-      <View>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
         <MedLog />
         <MedLog />
         <MedLog />
         <MedLog />
         <MedLog />
-      </View>
+      </ScrollView>
     )
   }
 }
 
 export default MedLogList;
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 20
+  }
+});
