2d78156d66d70fc529b3f33f47ef90b6e74252f2
Ankit <ankitrko7@gmail.com>
2017-10-21 23:16:09 -0700

    Mobile apis locally verified

--- webApp/hello.py
+++ webApp/hello.py
@@ -1,16 +1,28 @@
-from flask import Flask, session, redirect, url_for, escape, request, render_template
+from flask import Flask, session, redirect, url_for, escape, request, render_template, jsonify
 from pymongo import MongoClient
 import datetime
+import json
+from bson import ObjectId
+from bson.json_util import dumps
 app = Flask(__name__)
  
  
 
 def connect_to_db():
     # CreatePyMongoConnection
-    client = MongoClient("ds227865.mlab.com", 27865)
-    db = client['sunshinedb']
-    db.authenticate('admin', 'sunshine123')
-    return db,client
+	client = MongoClient()
+    #client = MongoClient("ds227865.mlab.com", 27865)
+	db = client['sunshinedb']
+    #db.authenticate('admin', 'sunshine123')r
+	return db,client
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 
 
 
@@ -74,7 +86,7 @@ def parentlogin():
 		
 @app.route('/logout', methods = ["POST"])
 def logout():
-   # remove the username from the session if it is there
+	# remove the username from the session if it is there
    if request.method == 'POST':
 		db,client = connect_to_db()
 		#session.pop('username', None)
@@ -86,15 +98,15 @@ def logout():
 #mobile app function
 @app.route('/parentlogout', methods=["POST"])
 def parentlogout():
-   # remove the username from the session if it is there
+	# remove the username from the session if it is there
 
    if request.method == 'POST':
-	   db, client = connect_to_db()
-	   # session.pop('username', None)
-	   print request.form['username']
-	   db.Parent.find_one_and_update({'username': request.form['username']}, {'$set': {'logged_in': False}})
-	   client.close()
-	   return "logged out"
+		db, client = connect_to_db()
+		# session.pop('username', None)
+		print request.form['username']
+		db.Parent.find_one_and_update({'username': request.form['username']}, {'$set': {'logged_in': False}})
+		client.close()
+		return "logged out"
 
 
 #mobileapp api
@@ -103,45 +115,60 @@ def getAllHouses():
 	if request.method == 'POST':
 		username = request.form["username"]
 		db,client = connect_to_db()
-		house_ids = db.Parents.find({"username":username},{'house_id'})
+		house_ids = db.Parents.find({"username":username},{'_id':0,'house_id':1})
 		all_houses = []
-		for house_id in house_ids:
-			all_houses.extend(db.houses.find({'house_id'},{'name', 'house_id'}))
+		for dict in house_ids:
+			k,house_id=dict.items()[0]
+
+			all_houses.extend(db.Houses.find({'house_id': house_id},{'_id':0,'name':1, 'house_id':1}))
 		client.close()
-		return all_houses
+		print all_houses
+		return jsonify(all_houses)
 	return None
 
 #mobileapp api
 @app.route('/getchildren', methods = ["POST"])
 def getAllChildren():
 	if request.method == 'POST':
-		house_id = request.form['house_id']
+		house_id = int(request.form['house_id'])
+		print type(house_id)
 		db,client = connect_to_db()
-		all_children = db.children.find({'house_id': house_id})
+		all_children = db.Children.find({'house_id': house_id},{'_id':0,"child_id":1, "first_name":1, "last_name":1})
+		list1=[]
+		for c in all_children:
+			list1.append(c)
+		print list1
 		client.close()
-		return all_children
+		return jsonify(list1)
 	return None
 
 @app.route('/getMedicationDetails', methods = ["POST"])
 def getMedicationDetails():
 	if request.method == 'POST':
-		child_id = request.form['child_id']
+		child_id = int(request.form['child_id'])
 		db,client = connect_to_db()
 		med_details = db.MedicineSchedule.find({'child_id': child_id,'AdministrationTime':{'$gte': datetime.datetime.now().replace(hour=0,minute =0, second = 0,  microsecond =0), '$lte':(datetime.datetime.now()+datetime.timedelta(days=1)).replace(hour=0, second=0, minute = 0,  microsecond=0)}})
+		list1 = []
+		for c in med_details:
+			list1.append(c)
 		client.close()
-		return med_details
+		print list1
+		return dumps(list1)
 	return None
+
 @app.route('/logMedicineGiven', methods = ["POST"])
 def logMedicineGiven():
 	if request.method == 'POST':
 		sched_id = request.form['schedule_id']
+		print sched_id
+		print type(sched_id)
 		db, client = connect_to_db()
-		med_details = db.MedicineSchedule.findAndModify({'_id': sched_id},{'$set':{'done':True}})
+		med_details = db.MedicineSchedule.find_and_modify({'_id': ObjectId(sched_id)},{'$set':{'done':True}})
 		client.close()
-		return True
-	return False
+		return "True"
+	return "False"
 
-
+'''
 def createChildren():
 	if request.method == 'POST':
 		if all(k in request.form for k in [first_name, last_name]):
@@ -157,7 +184,7 @@ def createChildren():
 		client.close()
 		return all_children
 	return None
-
+'''
 
 @app.route('/visualizations', methods = ["GET"])
 def getVisualizations():
